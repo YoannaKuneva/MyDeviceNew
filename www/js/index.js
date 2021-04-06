7eca37841f9682ffac24dd325d358e41de6d7fe5
YoannaKuneva <79211246+YoannaKuneva@users.noreply.github.com>
2021-04-06 18:38:26 +0300

    commit

--- www/js/index.js
+++ www/js/index.js
@@ -6,6 +6,7 @@ function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     window.addEventListener("batterystatus", onBatteryStatus, false);
     applyDeviceData();
+    checkConnection();
 }
 
 function applyDeviceData() {
@@ -23,4 +24,20 @@ function applyDeviceData() {
 function onBatteryStatus(status) {
     $("#batteryStatus").text(status.level);
     $("#isPlugged").text(status.isPlugged);
+}
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+    
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+    
+    $("#connectionType").text(states[networkState]);
 }
